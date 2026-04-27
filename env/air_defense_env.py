08f2f371dbccb2cb07b8ecfe1a20baddd5b6c994
FaithfulDao <victor.dubrana2@gmail.com>
2026-04-27 13:30:37 +0200

    feat: operating training gym env

--- env/air_defense_env.py
+++ env/air_defense_env.py
@@ -3,6 +3,7 @@ from gymnasium import spaces, Env
 import random
 import numpy as np
 import os
+from stable_baselines3 import PPO
 
 CLOSER=1
 INTERCEPTED=5
@@ -22,26 +23,29 @@ class AirDefenseEnv(Env):
         self.height = height
         self.length = length
         self.cumulative_reward = 0
-        self.state = [NOTHING] * (width*height*length)
-        self.target = (random.randrange(width), random.randrange(height), 0)
-        self.threat = (random.randrange(width), random.randrange(height), length-1)
-        self.interceptor = (random.randrange(width), random.randrange(height), 0)
-        self.reset()
 
-        while self.interceptor == self.target:
-            self.interceptor = (random.randrange(0, width), random.randrange(0, height), 0)
+        self.action_space = spaces.Discrete(6)
+
+        self.observation_space = spaces.Box(
+            low=0,
+            high=3,
+            shape=(width * height * length,),
+            dtype=np.int16
+        )
         
-        self.state[self._to_idx(*self.target)] = TARGET
-        self.state[self._to_idx(*self.threat)] = THREAT  
-        self.state[self._to_idx(*self.interceptor)] = INTERCEPTOR
+        self.reset()
         
         pass
 
     #returns the exact grid index idx
     def _to_idx(self, x, y, z):
         return x + y * self.width + z * self.width * self.height
+    
+    def _distance(self, a, b):
+        return np.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2 + (a[2]-b[2])**2)
 
     def step(self, action):
+        prev_distance = self._distance(self.interceptor, self.threat)
         tx, ty, tz = self.threat
         tax, tay, taz = self.target
 
@@ -54,6 +58,43 @@ class AirDefenseEnv(Env):
         tz -= 1 
 
         self.threat = (tx, ty, tz)
+
+        ix, iy, iz = self.interceptor
+
+        if action == 0: ix -= 1   # left
+        elif action == 1: ix += 1  # righ
+        elif action == 2: iy -= 1  # forward
+        elif action == 3: iy += 1  # backward
+        elif action == 4: iz -= 1  # descend
+        elif action == 5: iz += 1  # climb
+
+        ix = max(0, min(self.width - 1, ix))
+        iy = max(0, min(self.height - 1, iy))
+        iz = max(0, min(self.length - 1, iz))
+
+        self.interceptor = (ix, iy, iz)
+
+        if tz == 0:
+            reward = TARGET_HIT
+            done = True
+        elif self.interceptor == self.threat:
+            reward = INTERCEPTED
+            done = True
+        else:
+            new_distance = self._distance(self.interceptor, self.threat)
+            reward = CLOSER if new_distance < prev_distance else 0
+            done = False
+
+        self.state = [NOTHING] * (self.width * self.height * self.length)
+        self.state[self._to_idx(*self.target)] = TARGET
+        self.state[self._to_idx(*self.threat)] = THREAT
+        self.state[self._to_idx(*self.interceptor)] = INTERCEPTOR
+
+        self.cumulative_reward += reward
+        observation = np.array(self.state)
+        truncated = False
+        info = {}
+        return observation, reward, done, truncated, info
            
 
     def reset(self):    
@@ -66,12 +107,18 @@ class AirDefenseEnv(Env):
         self.state[self._to_idx(*self.threat)] = THREAT  
         self.state[self._to_idx(*self.interceptor)] = INTERCEPTOR
 
-        return np.array(self.state)
+        return np.array(self.state), {}
     
     def render(self):
         pretty_print(self.state, self.cumulative_reward)
 
 if __name__ == "__main__":
     env = AirDefenseEnv(5, 5, 5)
-    obs = env.reset()
-    print(obs)
+    obs, _ = env.reset()
+
+    for _ in range(100):
+        action = env.action_space.sample()
+        obs, reward, done, truncated, info = env.step(action)
+        print(f"reward: {reward}, done: {done}")
+        if done:
+            obs, _ = env.reset()  
